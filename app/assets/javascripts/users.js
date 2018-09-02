7d7fc2d612e0ac80644a4ef21c4e1e2de91bd1be
Brentwood Reid <bwoodreid@gmail.com>
2018-09-02 14:32:29 -0800

    Fix js slide functionality.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,11 +4,6 @@ $( document ).on('turbolinks:load', function() {
 	$("div.pio-status:contains('On Assignment')").addClass('onassignment');
 
 	$('div.card').click(function(){
-
-			$(this).find( "div.pio-details" ).toggle(function(){
-	    $(this).find( "div.pio-details" ).slideDown();
-	  },function(){
-	    $(this).find( "div.pio-details" ).slideUp();
-		});
+    	$(this).find( "div.details-container" ).slideToggle(200);
   });
-})
+});

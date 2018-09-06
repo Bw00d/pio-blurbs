f054dd4db373f4b6cd7c750ab11fd5fbdc357902
Brentwood Reid <bwoodreid@gmail.com>
2018-09-06 08:49:18 -0700

    Experiment with fixing the tags

--- app/assets/javascripts/blurbs.js
+++ app/assets/javascripts/blurbs.js
@@ -1,5 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function () {
-	
+	// fixed until
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 84px
+            });
+        }
+    });
+};
+
+$('#fixed-until').followTo(100);
+
 });

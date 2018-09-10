799e307593a8c1a7f92669ad3ba97fc42eb5ec73
Brentwood Reid <bwoodreid@gmail.com>
2018-09-10 10:54:16 -0700

    Add commas to taglist on edit form

--- app/models/blurb.rb
+++ app/models/blurb.rb
@@ -2,4 +2,8 @@ class Blurb < ApplicationRecord
 	acts_as_taggable # Alias for acts_as_taggable_on :tags
   # acts_as_taggable_on :content
   belongs_to :user
+
+  def stringify(tag_list)
+	  tag_list.inject('') { |memo, tag| memo += (tag + ',') }[0..-1]
+	end
 end

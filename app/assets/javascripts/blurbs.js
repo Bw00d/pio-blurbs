be37331b17b5fff9ee6c7ae549e82ef2a08fe2ab
Brentwood Reid <bwoodreid@gmail.com>
2018-09-10 09:32:33 -0700

    Add second header to place new blurb button in

--- app/assets/javascripts/blurbs.js
+++ app/assets/javascripts/blurbs.js
@@ -1,28 +1,6 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// $(document).on('turbolinks:load', function () {
-// 	// fixed until
-// var windw = this;
-
-// $.fn.followTo = function ( pos ) {
-//     var $this = this,
-//         $window = $(windw);
-    
-//     $window.scroll(function(e){
-//         if ($window.scrollTop() > pos) {
-//             $this.css({
-//                 position: 'absolute',
-//                 top: pos
-//             });
-//         } else {
-//             $this.css({
-//                 position: 'fixed',
-//                 top: 84px
-//             });
-//         }
-//     });
-// };
-
-// $('#fixed-until').followTo(100);
-
-// });
+$( document ).on('turbolinks:load', function() {
+	$(".author").hover(function() {
+		$(this).text("test");
+	});
+	
+});

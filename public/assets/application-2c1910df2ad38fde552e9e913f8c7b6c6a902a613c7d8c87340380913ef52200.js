83c2fb6e142deca43772b0656d67f1d060d1ecd2
Brentwood Reid <bwoodreid@gmail.com>
2018-09-10 20:57:08 -0700

    Precompile assets

--- public/assets/application-2c1910df2ad38fde552e9e913f8c7b6c6a902a613c7d8c87340380913ef52200.js
+++ public/assets/application-2c1910df2ad38fde552e9e913f8c7b6c6a902a613c7d8c87340380913ef52200.js
@@ -19672,34 +19672,22 @@ document.addEventListener("turbolinks:before-render", function(event) {
         node: event.data.newBody
     });
 });
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// $(document).on('turbolinks:load', function () {
-// 	// fixed until
-// var windw = this;
+$( document ).on('turbolinks:load', function() {
 
-// $.fn.followTo = function ( pos ) {
-//     var $this = this,
-//         $window = $(windw);
-    
-//     $window.scroll(function(e){
-//         if ($window.scrollTop() > pos) {
-//             $this.css({
-//                 position: 'absolute',
-//                 top: pos
-//             });
-//         } else {
-//             $this.css({
-//                 position: 'fixed',
-//                 top: 84px
-//             });
-//         }
-//     });
-// };
 
-// $('#fixed-until').followTo(100);
-
-// });
+	
+  $('.author-box').mouseover(function() {
+    $(this).children('.author').hide();
+    $(this).children(".author-name").show();
+ 
+  });
+  $('.author-box').mouseleave(function() {
+    $(this).children('.author').show();
+    $(this).children(".author-name").hide();
+ 
+  });
+	
+});
 (function() {
   (function() {
     (function() {

c26d1d48045f88ca668ee32c8657c902e8dc9705
Brentwood Reid <bwoodreid@gmail.com>
2018-10-03 10:44:08 -0700

    Add s3 configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb.
 
+  #Configuration defaults for s3 upload
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
   # Bullet gem
   config.after_initialize do
     Bullet.enable        = true
